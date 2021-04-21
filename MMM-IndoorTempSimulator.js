9f394019d37328ad58d6641230f0dcb1a6d21172
DavidFSPro <82946682+DavidFSPro@users.noreply.github.com>
2021-04-21 13:53:57 +0200

    Add files via upload

---
+++ MMM-IndoorTempSimulator.js
@@ -0,0 +1,61 @@
+/* Magic Mirror
+ * Module: MMM-IndoorTemperature
+ *
+ * By David M
+ * FabLab LabHidouille by Familles Solidaires.
+ */
+Module.register("MMM-IndoorTempSimulator", 
+{
+	// Default module config.
+	defaults: 
+	{
+		temp: 20,
+	},
+
+	start: function() 
+	{
+		Log.info('Starting module: ' + this.name);
+		this.tempRandom = this.config.temp;
+	},
+
+	// Override dom generator.
+	//getDom: function() 
+	//{
+	//	var DivName = document.createElement("div");
+	//	DivName.className = "Indoor_Temp";
+	//	DivName.innerHTML = "Temp : " + this.tempRandom + " °C";
+
+	//	return DivName;
+	//},
+
+	// messages reçus d'autres modules et du système (PAS de votre node_helper)
+	// le payload est une structure de données dépendant de la notification
+    notificationReceived: function(notification,payload,sender) 
+    {
+		var self = this;
+
+        switch(notification)
+        {
+            case "ALL_MODULES_STARTED": // notif de lancement de tous les modules
+				var firstTimer = setTimeout(()=>
+				{
+					this.sendNotification("INDOOR_TEMPERATURE", this.tempRandom);
+				}, 1*1000);	
+
+				var timer = setInterval(()=>
+				{
+					this.tempRandom = Math.random()*((this.config.temp+0.5)-(this.config.temp-0.5))+(this.config.temp-0.5);
+					this.sendNotification("INDOOR_TEMPERATURE", this.tempRandom); //envoi la notif à tout les modules "INDOOR_TEMPERATURE" et le paylaod "this.temp"
+				}, 30*1000);
+					
+			break;
+        }
+    },
+
+	//sendDataNotifications: function()
+	//{
+	//	var self = this;
+	//	self.sendNotification("INDOOR_TEMPERATURE", self.config.temp);
+	//},
+	
+});
